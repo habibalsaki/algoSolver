a5deaa6841a00e5da603b30248d81f1a077a466a
Habib Al Saki <habibalsaki@gmail.com>
2018-12-22 16:03:29 +0600

    Lesson 1 Iterations

---
+++ 1_Iterations/Iterations/solution/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace solution
+{
+    class Program
+    {
+        public int Solution(int n)
+        {
+            string binary = Convert.ToString(n,2);
+            binary = binary.Substring(binary.IndexOf('1'), 
+                    binary.LastIndexOf('1') - binary.IndexOf('1') + 1);
+
+            
+            string[] zeroRanges = binary.Split('1');
+
+            int max = 0;
+            foreach(var range in zeroRanges)
+            {
+                if(range.Length > max)
+                {
+                    max = range.Length;
+                }
+            }
+
+            return max;
+        }
+        static void Main(string[] args)
+        {
+            
+            var program = new Program();
+            Console.WriteLine(program.Solution(32));
+        }
+    }
+}
